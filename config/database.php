7bbe5240673767f35e610f96f0edc28eeaafbee9
Horemi Metho <horemizola@gmail.com>
2026-07-02 02:30:18 +0200

    Préparation du projet pour Render

--- config/database.php
+++ config/database.php
@@ -1,41 +1,21 @@
 <?php
+$host = getenv('DB_HOST') ?: 'localhost';
+$dbname = getenv('DB_NAME') ?: 'gestion_hospital';
+$user = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
 
-class Database
-{
-    private $host = "localhost";
-    private $dbname = "gestion_hospital";
-    private $username = "root";
-    private $password = "";
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $password
+    );
 
-    public $connexion;
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    public function connecter()
-    {
-        $this->connexion = null;
+} catch(PDOException $e){
 
-        try {
+    die("Erreur : ".$e->getMessage());
 
-            $this->connexion = new PDO(
-                "mysql:host={$this->host};dbname={$this->dbname};charset=utf8",
-                $this->username,
-                $this->password
-            );
-
-            // Gestion des erreurs
-            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // Retour des résultats sous forme de tableau associatif
-            $this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            // Désactivation de l'émulation des requêtes préparées
-            $this->connexion->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-        } catch (PDOException $e) {
-
-            die("Erreur de connexion : " . $e->getMessage());
-
-        }
-
-        return $this->connexion;
-    }
 }
+?>
